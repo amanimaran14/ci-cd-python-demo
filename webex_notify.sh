84a2100169cd66de851964505b37169bd8a41a68
amanimaran14 <amanimaran@cpp.edu>
2025-11-06 21:46:55 +0000

    FINAL SUCCESS ATTEMPT: Verified Bot status and updated token

--- webex_notify.sh
+++ webex_notify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. Define variables (Using your provided token and confirmed ID)
-WEBEX_TOKEN="MjdiZDdiMDUtNjU1Yi00ZjZhLWE1OTktMTRlODUxMGJmOWZhZDRhYjVkZDgtMjZl_P0A1_13494cac-24b4-4f89-8247-193cc92a7636"
+WEBEX_TOKEN="OWRkY2MyYTItZjc3ZS00MmFiLWI0OWItOTRlOTQzYTVmYWYxNGQ0NTc4MjctNGRh_P0A1_13494cac-24b4-4f89-8247-193cc92a7636"
 # Using the confirmed UUID format for the Space ID
 SPACE_ID="d0c54540-ba73-11f0-a71a-b7d267395f5d"
 
